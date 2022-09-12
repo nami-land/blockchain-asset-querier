e89b0f79270c68a9bf007b7b7874dd4edeeb67b9
imokoi <tiantengfei@outlook.com>
2022-09-12 17:51:02 +0900

    add hello world demo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::net::SocketAddr;
+
+use axum::{Router, routing::get};
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    let app = Router::new()
+        .route("/", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
+    println!("web server is listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "hello world"
 }
