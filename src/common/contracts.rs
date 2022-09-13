3cb2228ca4dc8792eb584231bea13f3a7da052ba
imokoi <tiantengfei@outlook.com>
2022-09-13 14:08:45 +0900

    implement code to read contracts

--- src/common/contracts.rs
+++ src/common/contracts.rs
@@ -1,41 +1,74 @@
-use super::defines::{SupportedContract, NetworkType};
+use super::defines::{SupportedContract, NetworkType, Error};
+use ethers::prelude::abigen;
+use ethers_core::{
+    abi::Abi,
+    types::{Address, H256},
+};
 
 #[derive(Debug, Default)]
-struct AddressManager {}
+pub struct AddressManager {}
 
 impl AddressManager {
-    fn get_contract_address(&self, contract_type: SupportedContract, network_type: NetworkType) -> &str {
+    pub fn get_contract_address(&self, contract_type: SupportedContract, network_type: NetworkType) -> Result<Address, Error> {
         match contract_type {
             SupportedContract::BUSDTokenContract => {
                 match network_type {
-                    NetworkType::BSCMainNetwork => "0x2D6C8229E1e14F4D35037F977e5486EE1Bfa0190",
-                    NetworkType::BSCTestNetwork => "0x2D6C8229E1e14F4D35037F977e5486EE1Bfa0190"
+                    NetworkType::BSCMainNetwork => Ok("0x2D6C8229E1e14F4D35037F977e5486EE1Bfa0190".parse::<Address>()?),
+                    NetworkType::BSCTestNetwork => Ok("0x2D6C8229E1e14F4D35037F977e5486EE1Bfa0190".parse::<Address>()?)
                 }
             },
             SupportedContract::NECOTokenContract => {
                 match network_type {
-                    NetworkType::BSCMainNetwork => "0xd23891FC1A515A88C571064637502e3766819e2d",
-                    NetworkType::BSCTestNetwork => "0xafA98d54481a9aE468AB21b9268609fF50795795"
+                    NetworkType::BSCMainNetwork => Ok("0xd23891FC1A515A88C571064637502e3766819e2d".parse::<Address>()?),
+                    NetworkType::BSCTestNetwork => Ok("0xafA98d54481a9aE468AB21b9268609fF50795795".parse::<Address>()?),
                 }
             },
             SupportedContract::NFISHTokenContract => {
                 match network_type {
-                    NetworkType::BSCMainNetwork => "0xa0c72B1F89531b6BD61C640d03Bd4507773C0cfC",
-                    NetworkType::BSCTestNetwork => "0xa0c72B1F89531b6BD61C640d03Bd4507773C0cfC"
+                    NetworkType::BSCMainNetwork => Ok("0xa0c72B1F89531b6BD61C640d03Bd4507773C0cfC".parse::<Address>()?),
+                    NetworkType::BSCTestNetwork => Ok("0xa0c72B1F89531b6BD61C640d03Bd4507773C0cfC".parse::<Address>()?),
                 }
             },
             SupportedContract::NecoNFTContract => {
                 match network_type {
-                    NetworkType::BSCMainNetwork => "0xEA5534Bac1291676595223579517D35Ad9C382eE",
-                    NetworkType::BSCTestNetwork => "0xEB1C424A31490A9B141126838a3c625647f22BDc"
+                    NetworkType::BSCMainNetwork => Ok("0xEA5534Bac1291676595223579517D35Ad9C382eE".parse::<Address>()?),
+                    NetworkType::BSCTestNetwork => Ok("0xEB1C424A31490A9B141126838a3c625647f22BDc".parse::<Address>()?),
                 }
             },
             SupportedContract::StakeNecoForFeeContract => {
                 match network_type {
-                    NetworkType::BSCMainNetwork => "0x8bfB9140658632239f8a1450955cB5bD7Ce586ED",
-                    NetworkType::BSCTestNetwork => "0xa4329D80BE20813CbfeF5B2e593CA2893441E2dd"
+                    NetworkType::BSCMainNetwork => Ok("0x8bfB9140658632239f8a1450955cB5bD7Ce586ED".parse::<Address>()?),
+                    NetworkType::BSCTestNetwork => Ok("0xa4329D80BE20813CbfeF5B2e593CA2893441E2dd".parse::<Address>()?),
                 }
             },
         }
     }
 }
+
+#[derive(Debug, Default)]
+struct AbiManager {}
+
+// impl AbiManager {
+//     fn get_contract_abi(&self, contract_type: SupportedContract) -> Result<Abi, Error> {
+//         match contract_type {
+//             SupportedContract::BUSDTokenContract => {
+//                 abigen!(
+//                     BUSDToken,
+//                     "./abis/erc20.json",
+//                 )
+//             },
+//             SupportedContract::NFISHTokenContract => {
+
+//             }
+//             SupportedContract::NECOTokenContract => {
+
+//             }
+//             SupportedContract::NecoNFTContract => {
+
+//             }
+//             SupportedContract::StakeNecoForFeeContract => {
+
+//             }
+//         }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 mod common;
 
+use std::fs;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
-use crate::common::defines::{BSC_MAIN_NETWORK_RPC, BSC_TEST_NETWORK_RPC};
+use crate::common::defines::{
+    Error, SupportedContract, BSC_MAIN_NETWORK_RPC, BSC_TEST_NETWORK_RPC,
+};
 use axum::{routing::get, Router};
-use ethers::providers::{Http, Provider};
+use common::{
+    contracts::AddressManager,
+    defines::{NetworkType, SupportedERC20Token},
+};
+use ethers::abi::Abi;
+use ethers::contract::Contract;
+use ethers::{
+    prelude::{abigen, SignerMiddleware},
+    providers::{Http, Provider},
+};
+
+abigen!(
+    ERC20Contract,
+    "./src/abis/erc20.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     let app = Router::new().route("/", get(hello_world));
 
     let bsc_main_client = Provider::<Http>::try_from(BSC_MAIN_NETWORK_RPC).unwrap();
     let bsc_test_client = Provider::<Http>::try_from(BSC_TEST_NETWORK_RPC).unwrap();
+
+    let abi_str = fs::read_to_string("./src/abis/erc20.json").unwrap();
+    let abi: Abi = serde_json::from_str(&abi_str).unwrap();
+
+    let address = AddressManager::default()
+        .get_contract_address(
+            SupportedContract::NECOTokenContract,
+            NetworkType::BSCTestNetwork,
+        )
+        .unwrap();
+    let contract = Contract::new(address, abi, bsc_test_client);
+
+    let symbol: String = contract.method::<_, String>("symbol", ())?.call().await?;
+    println!("symbol is {}", symbol);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
     println!("web server is listening on {}", addr);
@@ -19,6 +52,8 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
 async fn hello_world() -> &'static str {
