57e35e203b038789a6fbc53b3f9de5d224eacb52
imokoi <tiantengfei@outlook.com>
2022-10-11 14:05:14 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use std::fs;
+use std::net::SocketAddr;
+
+use ethers::providers::{Http, Provider};
+use log::{info, LevelFilter};
+use once_cell::sync::OnceCell;
+
+use common::defines::NetworkType;
+use common::provider::ProviderManager;
+
+use crate::common::defines::{Error, BSC_MAIN_NETWORK_RPC, BSC_TEST_NETWORK_RPC};
+use crate::models::{Address, AddressConfig};
+
 mod apis;
 mod common;
 mod models;
 mod router;
 mod services;
-
-use crate::common::defines::{Error, BSC_MAIN_NETWORK_RPC, BSC_TEST_NETWORK_RPC};
-use crate::models::{Address, AddressConfig};
-use common::defines::NetworkType;
-use common::provider::ProviderManager;
-use ethers::providers::{Http, Provider};
-use log::{info, LevelFilter};
-use once_cell::sync::OnceCell;
-use std::fs;
-use std::net::SocketAddr;
 
 static ADDRESS_MANAGER: OnceCell<AddressConfig> = OnceCell::new();
 
@@ -21,13 +24,6 @@ async fn main() -> Result<(), Error> {
     pretty_env_logger::formatted_timed_builder()
         .filter_level(LevelFilter::Info)
         .init();
-
-    // let env_args: HashMap<String, String> = env::vars().collect();
-    // let env = env_args.get("env");
-    // match env {
-    //     Some(env) => println!("env: {}", env),
-    //     None => println!("env is none"),
-    // };
 
     let address_content = fs::read_to_string("address.toml")?;
     let address: AddressConfig = toml::from_str(&address_content)?;
